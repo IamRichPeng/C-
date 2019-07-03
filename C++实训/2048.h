47eccdf1befe4092fa6f09f6c115ad9e3415dcdb
pradawa <rpby6@mail.missouri.edu>
2019-07-03 10:14:08 -0500

    Move to head file and build Menu

---
+++ C++实训/2048.h
@@ -0,0 +1,138 @@
+//
+//  2048.h
+//  C++实训
+//
+//  Created by 彭睿诚 on 2019/7/3.
+//  Copyright © 2019年 Ruicheng Peng. All rights reserved.
+//
+
+#ifndef _048_h
+#define _048_h
+
+#include<iostream>
+#include <ctime>
+#include <cstdlib>
+#include "curses.h"
+#include <iomanip>
+
+using namespace std;
+
+//棋盘
+int board[4][4];
+
+//方向：下（0） 右（1） 上（2） 左（3）,省去用四种case。
+int dirRow[] = {1,0,-1,0};
+int dirColumn[] = {0,1,0,-1};
+
+//找一个空白位置
+pair<int, int> generateUnoccupiedPosition(){
+    int occupied,row,column;
+    occupied = 1;
+    while(occupied){
+        row = rand() % 4;
+        column = rand() % 4;
+        if(board[row][column] == 0){
+            occupied = 0;
+        }
+    }
+    return make_pair(row,column);
+}
+
+int randomNum(){
+    int a = rand() % 2;
+    if( a == 0)
+        return 3;
+    else
+        return 6;
+}
+
+//重开游戏，
+void newGame(){
+    for(int i = 0;i < 4; ++i){
+        for(int j = 0;j <4;++j){
+            board[i][j] = 0 ;
+        }
+    }
+    pair<int, int> pos = generateUnoccupiedPosition();
+    board[pos.first][pos.second] = randomNum();
+    
+}
+
+//输出用户界面
+void printUI(){
+    system("clear"); //clear screen
+    for(int i =0;i < 4; ++i){
+        for(int j =0; j<4;++j)
+            if(board[i][j]==0)
+                cout<< setw(4)<<"."; // simple way to align 对齐
+            else
+                cout<< setw(4)<< board[i][j];
+        cout<<"\n";
+    }
+    cout<<"N: new game, A: left S: down, D: right , W:up, Q: quit\n";
+}
+
+//上下左右移动！！**********************************************************
+
+bool canDoMove(int row, int column, int nextRow, int nextColumn){
+    if (nextRow < 0 || nextColumn < 0 || nextRow>=4 || nextColumn >= 4){
+        return  false; // outside the board
+    }
+    if(board[row][column] == 0){
+        return false; // 该位为零！！不然就会无止尽移动
+    }
+    if (board[row][column] != board[nextRow][nextColumn] && board[nextRow][nextColumn]!= 0){
+        return false; //different numbers 不同数字
+    }
+    return true;
+}
+
+void applyMove(int direction){
+    // default上，左
+    int startRow = 0, startColumn = 0;
+    int rowStep = 1, columnStep = 1;
+    
+    //下
+    if (direction == 0){
+        startRow = 3;
+        rowStep = -1;
+    }
+    
+    //右
+    if(direction == 1){
+        startColumn = 3;
+        columnStep = -1;
+    }
+    
+    //用循环实现：移动一次，所有数字向该方向移动到不可移动为止。
+    int movePossible, canAddition = 0;
+    
+    do{
+        movePossible = 0;
+        for(int i = startRow;i >= 0 && i < 4; i += rowStep)
+            for(int j = startColumn; j >= 0 && j < 4; j += columnStep){
+                int nextI = i + dirRow[direction];
+                int nextJ = j + dirColumn[direction];
+                
+                //cout<<i<<" "<<j<<" "<<nextI<<" "<<nextJ<<"\n";
+                //check conditions!
+                if (canDoMove(i,j,nextI,nextJ)){
+                    board[nextI][nextJ] += board[i][j];
+                    board[i][j] = 0;
+                    movePossible = 1;
+                    canAddition = 1;
+                }
+            }
+    }
+    while(movePossible);
+    
+    //do-while全部移动后，并产生新的数字
+    if(canAddition){
+        pair<int, int> pos = generateUnoccupiedPosition();
+        board[pos.first][pos.second] = randomNum();
+    }
+    
+    
+}
+
+#endif /* _048_h */
--- C++实训/main.cpp
+++ C++实训/main.cpp
@@ -11,161 +11,79 @@
 #include <cstdlib>
 #include "curses.h"
 #include <iomanip>
+#include "2048.h"
 
 using namespace std;
 
-//棋盘
-int board[4][4];
-
-//方向：下（0） 右（1） 上（2） 左（3）,省去用四种case。
-   int dirRow[] = {1,0,-1,0};
-int dirColumn[] = {0,1,0,-1};
-
 //找一个空白位置
-pair<int, int> generateUnoccupiedPosition(){
-    int occupied,row,column;
-    occupied = 1;
-    while(occupied){
-        row = rand() % 4;
-        column = rand() % 4;
-        if(board[row][column] == 0){
-            occupied = 0;
-        }
-    }
-        return make_pair(row,column);
-    }
-
+pair<int, int> generateUnoccupiedPosition();
 //重开游戏，
-void newGame(){
-    for(int i = 0;i < 4; ++i){
-        for(int j = 0;j <4;++j){
-            board[i][j] = 0 ;
-    }
-    }
-    pair<int, int> pos = generateUnoccupiedPosition();
-    board[pos.first][pos.second] = 2;
-    
-}
-
+void newGame();
 //输出用户界面
-void printUI(){
-    system("clear"); //clear screen
-    for(int i =0;i < 4; ++i){
-        for(int j =0; j<4;++j)
-            if(board[i][j]==0)
-                cout<< setw(4)<<"."; // simple way to align 对齐
-            else
-                cout<< setw(4)<< board[i][j];
-        cout<<"\n";
-    }
-    cout<<"N: new game, A: left S: down, D: right , W:up, Q: quit\n";
-}
-
+void printUI();
 //上下左右移动！！**********************************************************
-
-bool canDoMove(int row, int column, int nextRow, int nextColumn){
-    if (nextRow < 0 || nextColumn < 0 || nextRow>=4 || nextColumn >= 4){
-        return  false; // outside the board
-    }
-    if(board[row][column] == 0){
-        return false; // 该位为零！！不然就会无止尽移动
-    }
-    if (board[row][column] != board[nextRow][nextColumn] && board[nextRow][nextColumn]!= 0){
-        return false; //different numbers 不同数字
-    }
-    return true;
-}
-
-void applyMove(int direction){
-   // default上，左
-    int startRow = 0, startColumn = 0;
-    int rowStep = 1, columnStep = 1;
-    
-    //下
-    if (direction == 0){
-        startRow = 3;
-        rowStep = -1;
-    }
-    
-    //右
-    if(direction == 1){
-        startColumn = 3;
-        columnStep = -1;
-    }
-    
-    //用循环实现：移动一次，所有数字向该方向移动到不可移动为止。
-    int movePossible, canAddition = 0;
-    
-    do{
-        movePossible = 0;
-        for(int i = startRow;i >= 0 && i < 4; i += rowStep)
-            for(int j = startColumn; j >= 0 && j < 4; j += columnStep){
-                int nextI = i + dirRow[direction];
-                int nextJ = j + dirColumn[direction];
-                
-                //cout<<i<<" "<<j<<" "<<nextI<<" "<<nextJ<<"\n";
-                //check conditions!
-                if (canDoMove(i,j,nextI,nextJ)){
-                    board[nextI][nextJ] += board[i][j];
-                    board[i][j] = 0;
-                    movePossible = 1;
-                    canAddition = 1;
-            }
-        }
-    }
-    while(movePossible);
-    
-    //do-while全部移动后，并产生新的数字
-    if(canAddition){
-        pair<int, int> pos = generateUnoccupiedPosition();
-        board[pos.first][pos.second] = 2;
-    }
-    
-    
-}
-
-
-
-
-
-
+bool canDoMove(int , int , int , int);
+void applyMove(int);
 
 
 
 int main(){
     
-    //generate a random value for Generating new numbers at randam position
-    srand(time(0));
-
-    //把用户输入的asdw指令转化为简单int
-    char resignDir[128];
-    resignDir['s'] = 0;//下
-    resignDir['d'] = 1;//右
-    resignDir['w'] = 2;//上
-    resignDir['a'] = 3;//左
+    char gameMode;
+    //Print Menu
+    cout<< " Pick a game you can't win\n";
+    cout<< "1. Hysterical 2048; 2.A bot u can't beat 3.Exit game\n";
+    cin >> gameMode;
     
-    newGame();
-    
-    while(true){
-        printUI();
-        char input;
-        cin >>input;
+    while( gameMode != 51){
         
-        if (input == 'n')
-            newGame();
-        else if (input == 'q')
-            break;
-        else{
-            int currentDirection = resignDir[input];
+    switch (gameMode) {
+        case 49:
+            //generate a random value for Generating new numbers at randam position
+            srand(time(0));
             
-            //移动函数
-            applyMove(currentDirection);
-        }
+            //把用户输入的asdw指令转化为简单int
+            char resignDir[128];
+            resignDir['s'] = 0;//下
+            resignDir['d'] = 1;//右
+            resignDir['w'] = 2;//上
+            resignDir['a'] = 3;//左
+            
+            newGame();
+            
+            while(true){
+                printUI();
+                char input;
+                cin >>input;
+                
+                if (input == 'n')
+                    newGame();
+                else if (input == 'q')
+                    break;
+                else{
+                    int currentDirection = resignDir[input];
+                    
+                    //移动函数
+                    applyMove(currentDirection);
+                }
+                
+            }
+            
+            break;
+            
+        case 50:
+            break;
+        default:
+            cout<<"Are u Serious? Put a valid number!\n\n";
+            break;
+    }
+        //reprint menu
+        cout<< " Pick a game you can't win\n";
+        cout<< "1. Hysterical 2048; 2.A bot u can't beat 3.Exit game\n";
+        cin >> gameMode;
         
     }
     
-    
-
     return 0;
   
 }
